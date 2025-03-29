11b067a3e0e88c9c8c0997621cd7ef3a27b5d008
sidorovichva <sidorovichva@gmail.com>
2025-03-29 18:19:54 +0300

    get all main entries

--- src/main/java/com/sid/springintegration/MainController.java
+++ src/main/java/com/sid/springintegration/MainController.java
@@ -1,17 +1,22 @@
 package com.sid.springintegration;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/main")
 public class MainController {
 
     private final MainService service;
+
+    @GetMapping("/getAll")
+    public List<MainEntry> getAllEntries() {
+        return service.getAllEntries();
+    }
 
     @PostMapping("/add")
     public void addMainEntry(@RequestParam String name) {
--- src/main/java/com/sid/springintegration/MainService.java
+++ src/main/java/com/sid/springintegration/MainService.java
@@ -3,11 +3,17 @@ package com.sid.springintegration;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class MainService {
 
     private final MainEntryRepository repository;
+
+    public List<MainEntry> getAllEntries() {
+        return repository.findAll();
+    }
 
     public void addMainEntry(MainEntry entry) {
         repository.save(entry);
